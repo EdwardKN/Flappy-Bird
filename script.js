45139f0a97b40c4a9ac4872cfb6ae818a9e2c929
Edward <edward.klinghammer@gmail.com>
2022-10-05 12:45:38 +0200

    semicolon för fan

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var images = {
         src:"./images/background.png" ,
         offset:0
     }
-}
+};
 
 Object.entries(images).forEach(image => {
     image[1].img.src = image[1].src;
@@ -41,10 +41,10 @@ function init(){
     };
     player = new Player(canvas.width/10,canvas.height/3,canvas.width/15,canvas.height/10,0.5* constants.sizeConstant.y,0);
     createPipe();
-}
+};
 function createPipe(){
     pipeArray.push(new Pipe(0,0,canvas.width/10,canvas.height/2.9,canvas.height/2.9 ,Math.random()*canvas.height/3-Math.random()*canvas.height/6))
-}
+};
 
 function Player(x,y,width,height,weight,velocityY){    
         this.x = x;
@@ -60,7 +60,7 @@ function Player(x,y,width,height,weight,velocityY){
         this.angle = 45;
         
         this.draw = function(){
-            drawRotatedImage(this.x,this.y,this.width,this.height, images.player.img,this.angle)
+            drawRotatedImage(this.x,this.y,this.width,this.height, images.player.img,this.angle);
                                        
             c.fillStyle = "black";
             c.font = "30px Arial";
@@ -72,19 +72,19 @@ function Player(x,y,width,height,weight,velocityY){
                 this.angle = -this.velocityY*2;
             }else{
                 this.angle = -this.velocityY;
-            }
+            };
             if(this.angle > 90){
                 this.angle = 90;
-            }
+            };
 
             if(this.started === true && this.y + this.height < canvas.height){
                 this.velocityY -= constants.gravity / constants.sizeConstant.y;
                 this.y -= this.velocityY*this.weight;
-            }
+            };
             if(this.y + this.height > canvas.height -20){
                 this.angle = 90;
                 this.y = canvas.height-this.width;
-            }
+            };
             
             this.draw();
             
@@ -96,31 +96,17 @@ function Player(x,y,width,height,weight,velocityY){
     
 };
 
-function drawRotatedRectangle(x,y,w,h,color,angle){
-    c.fillStyle = color;
-    let degree = angle * Math.PI / 180
-    let middlePoint = {
-        x:x+w/2,
-        y:y+h/2
-    };
-    c.save();
-    c.translate(middlePoint.x,middlePoint.y)
-    c.rotate(degree);
-    c.fillRect(-w/2,-h/2,w,h);
-    c.restore();
-}
 function drawRotatedImage(x,y,w,h,img,angle,mirrored){
-    let degree = angle * Math.PI / 180
+    let degree = angle * Math.PI / 180;
     let middlePoint = {
         x:x+w/2,
         y:y+h/2
     };
     c.save();
-    c.translate(middlePoint.x,middlePoint.y)
+    c.translate(middlePoint.x,middlePoint.y);
     c.rotate(degree);
     if(mirrored === true){
         c.scale(-1, 1);
-
     }
 
     c.drawImage(img,-w/2,-h/2,w,h);
@@ -140,12 +126,9 @@ function Pipe(x,y,width,heightUpper,heightBottom,gapPosition){
     this.givenPoint = false;
     this.color = "gray";
 
-    this.draw = function(){
-        c.fillStyle = this.color;
-    
-        drawRotatedImage(canvas.width-this.x,-100,this.width,this.heightUpper-this.gapPosition+100,images.pipe.img,180,true)
-        drawRotatedImage(canvas.width-this.x,canvas.height-this.heightBottom-gapPosition,(this.width),this.heightBottom + gapPosition +50,images.pipe.img,0)
-        
+    this.draw = function(){    
+        drawRotatedImage(canvas.width-this.x,-100,this.width,this.heightUpper-this.gapPosition+100,images.pipe.img,180,true);
+        drawRotatedImage(canvas.width-this.x,canvas.height-this.heightBottom-gapPosition,(this.width),this.heightBottom + gapPosition +50,images.pipe.img,0);
     };
 
     this.update = function(){
@@ -166,27 +149,22 @@ function Pipe(x,y,width,heightUpper,heightBottom,gapPosition){
             if(detectCollition(player.x+ player.width/2,0,player.width,5,canvas.width-this.x+this.width/2,0,this.width,5) && this.givenPoint === false){
                 this.givenPoint = true;
                 player.points++;
-            }
-        }
-
+            };
+        };
     };
-}
+};
 
 function detectCollition(x,y,w,h,x2,y2,w2,h2){
-    if(x+w>x2 &&
-        x<x2+w2 &&
-        y+h>y2 &&
-        y<y2+h2
-        ){
+    if(x+w>x2 && x<x2+w2 && y+h>y2 && y<y2+h2){
             return true;
-        }
-}
+        };
+};
 
 function animate(){
     requestAnimationFrame(animate);
-    c.clearRect(0,0,canvas.width,canvas.height)
-    c.drawImage(images.background.img,images.background.offset%canvas.width,0,canvas.width,canvas.height)
-    c.drawImage(images.background.img,images.background.offset%canvas.width + canvas.width-1,0,canvas.width,canvas.height)
+    c.clearRect(0,0,canvas.width,canvas.height);
+    c.drawImage(images.background.img,images.background.offset%canvas.width,0,canvas.width,canvas.height);
+    c.drawImage(images.background.img,images.background.offset%canvas.width + canvas.width-1,0,canvas.width,canvas.height);
     if(player.dead === true){
         deadTimer++;
         pipeArray.forEach(Pipe=> {
@@ -199,9 +177,9 @@ function animate(){
         });
         if(player.started === true){
             images.background.offset -=(1+player.points/20)*constants.sizeConstant.x;
-        }
+        };
 
-    }
+    };
     
     player.update();
 };
@@ -209,24 +187,23 @@ function animate(){
 
 window.addEventListener("keyup",function(event){
     jump();
-})
+});
 window.addEventListener("mouseup",function(event){
     jump();
-    
-})
+});
 function jump(){
     player.started = true;
     if(player.y > canvas.height/15 && player.dead === false){
         player.velocityY = 18;
-    }
+    };
     if(player.dead === true && player.y + player.height >= canvas.height-20){
         init();
-    }
-}
+    };
+};
 
 window.addEventListener("resize",function(){
     init();
-})
+});
 
 init();
 animate();
